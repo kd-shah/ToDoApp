f5769517788d358877a478bc780d07c4c25460cd
kshah53 <kunal.shah@concordusa.com>
2023-02-10 14:24:59 +0530

    new features added 2

--- src/App.js
+++ src/App.js
@@ -1,147 +1,119 @@
-import './App.scss';
-import { useState, useEffect } from 'react';
-import {InputView} from './input.js';
-import {Button} from './Button.js';
-import { ToDoItem } from './ListItem.js';
+import "./App.scss";
+import { useState } from "react";
+import { InputView } from "./input.js";
+import { Button } from "./Button.js";
+import { ToDoItem } from "./ListItem.js";
 import { FaChevronUp } from "react-icons/fa";
 import { FaChevronDown } from "react-icons/fa";
-import { AiFillDelete} from "react-icons/ai";
-
+import { AiFillDelete } from "react-icons/ai";
+import { AiOutlineInfoCircle } from "react-icons/ai";
 
 export default function TODO() {
+  const [ToDoItems, setToDoItem] = useState([
+    { id: 0, item: "First Task", isActive: true },
+  ]);
 
-    const [ToDoItems, setToDoItem] = useState([
-        {id: 0, item: 'First Task' }
-    ])
+  const [newToDoItem, setNewToDoItem] = useState("");
 
-    const [newToDoItem, setNewToDoItem] = useState('')
+  //   useEffect(()=> {
+  //    setToDoItem(ToDoItems)
+  //   },[ToDoItems])
+  const Add = () => {
+    if (newToDoItem !== "") {
+      setToDoItem([
+        ...ToDoItems,
+        { id: ToDoItems.length, item: newToDoItem, isActive: true },
+      ]);
 
-  /*  const [style, setStyle] = useState("list");*/
+      setNewToDoItem("");
+    } else alert("Invalid");
+  };
 
-   
+  const ClearAll = () => {
+    setToDoItem([]);
+  };
 
-    const Add = () => {
-        if (newToDoItem !== "") {
-            setToDoItem([...ToDoItems, { id: ToDoItems.length , item: newToDoItem }])
-            
-            setNewToDoItem('')
-        }
-        else alert("Invalid")   
+  const onChangeEvent = (e) => {
+    setNewToDoItem(e.target.value);
+  };
+
+  const Clear = (id1) => {
+    const newList = ToDoItems.filter((taskitem) => taskitem.id !== id1);
+    setToDoItem(newList);
+  };
+
+  const Up = (u) => {
+    const ind = ToDoItems.findIndex((taskitem) => taskitem.id === u);
+    let tempObj = ToDoItems[ind - 1];
+    ToDoItems[ind - 1] = ToDoItems[ind];
+    ToDoItems[ind] = tempObj;
+    setToDoItem([...ToDoItems]);
+  };
+
+  const Status = (a) => {
+    const ind = ToDoItems.findIndex((taskitem) => taskitem.id === a);
+
+    if (ToDoItems[ind].isActive === true) {
+      ToDoItems[ind].isActive = false;
+    } else {
+      ToDoItems[ind].isActive = true;
     }
 
-    
+    console.log(ToDoItems[ind]);
+    setToDoItem([...ToDoItems]);
+  };
+  const Down = (d) => {
+    const ind = ToDoItems.findIndex((taskitem) => taskitem.id === d);
 
-    const ClearAll = () => {
-        setToDoItem([])
-    }
+    /*ToDoItems[ind] = ToDoItems.splice(ind+1, 1, ToDoItems[ind])[0];*/
 
-    const onChangeEvent = (e) => {
-        
-        setNewToDoItem(e.target.value)
-    }
-
-    const Clear = (id1) => {
-        const newList = ToDoItems.filter(taskitem => taskitem.id !== id1);
-        
-        setToDoItem(newList);
-    }
-
-
-    
-
-    
- const Up = (u) => {
- const ind = ToDoItems.findIndex(taskitem => taskitem.id === u);
-            let tempObj = ToDoItems[ind - 1];
-            ToDoItems[ind - 1] = ToDoItems[ind];
-            ToDoItems[ind] = tempObj;
-        }
-            
-       
-
-
-        
-    
-
-
-    const Down = (d) => {
-        const ind = ToDoItems.findIndex(taskitem => taskitem.id === d);
-        /* console.log(ind);*/
-
-        /*ToDoItems[ind] = ToDoItems.splice(ind+1, 1, ToDoItems[ind])[0];*/
-
-        /*let tempObj = ToDoItems.splice(ind, 1, ToDoItems[ind+1])[0];
+    /*let tempObj = ToDoItems.splice(ind, 1, ToDoItems[ind+1])[0];
         ToDoItems.splice(ind+1, 1, tempObj);*/
 
-       
-            let tempObj = ToDoItems[ind + 1];
-            ToDoItems[ind + 1] = ToDoItems[ind];
-            ToDoItems[ind] = tempObj;
+    let tempObj = ToDoItems[ind + 1];
+    ToDoItems[ind + 1] = ToDoItems[ind];
+    ToDoItems[ind] = tempObj;
 
-            console.log(ToDoItems[ind]);
-            setToDoItem(ToDoItems);        
-    }
+    console.log(ToDoItems[ind]);
+    setToDoItem([...ToDoItems]);
+  };
 
-   
-
-  
-
-     return (
-
-      <>
-       <div className="background">
+  return (
+    <>
+      <div className="background">
         <h1>TODO LIST</h1>
         <InputView
-                className = "input"
-                value={newToDoItem}
-                placeholder={"Enter Text here"}
-                onChange={onChangeEvent}
+          className="input"
+          value={newToDoItem}
+          placeholder={"Enter Text here"}
+          onChange={onChangeEvent}
         />
-        <Button 
-                name={"Add"}
-                onClick={Add}            
-        />
-        <Button 
-                name={"Clear All"}
-                onClick={ClearAll}
-        />
-        </div>
-        
+        <Button name={"Add"} onClick={Add} />
+        <Button name={"Clear All"} onClick={ClearAll} />
+      </div>
 
-            {
-                ToDoItems.map((tasks) => {
-                    return (
-                        <div className="list">
+      {ToDoItems.map((tasks) => {
+        return (
+          <div
+            className={tasks.isActive ? "list" : "inActiveTask"}
+            name="list"
+            key={tasks.id}
+          >
+            <ToDoItem value={tasks.item} />
 
-                           
-                            <ToDoItem
-                                value={tasks.item}
-                                                           
-                            />
-                            
+            <AiFillDelete className="buttons" onClick={() => Clear(tasks.id)} />
 
-                           <AiFillDelete className="buttons"
-                                onClick={() => Clear(tasks.id)}
-                           />                   
+            <AiOutlineInfoCircle
+              className="buttons"
+              onClick={() => Status(tasks.id)}
+            />
 
-                           <FaChevronDown className="buttons"
-                                onClick={() => Down(tasks.id)}
-                           />
+            <FaChevronDown className="buttons" onClick={() => Down(tasks.id)} />
 
-                           <FaChevronUp className="buttons"
-                                onClick={() => Up(tasks.id)}
-                            />
-
-                            
-                           
-
-                        </div>
-                            );
-
-                })
-                }
-       
-          
-        </>
-    )
+            <FaChevronUp className="buttons" onClick={() => Up(tasks.id)} />
+          </div>
+        );
+      })}
+    </>
+  );
 }
