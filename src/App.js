6aaee0202f0ee36ddea2a92e1a6e3357f8d075a1
kshah53 <kunal.shah@concordusa.com>
2023-02-13 12:01:01 +0530

    UI changes and bug fixes

--- src/App.js
+++ src/App.js
@@ -15,9 +15,7 @@ export default function TODO() {
 
   const [newToDoItem, setNewToDoItem] = useState("");
 
-  //   useEffect(()=> {
-  //    setToDoItem(ToDoItems)
-  //   },[ToDoItems])
+  //Adding a Task
   const Add = () => {
     if (newToDoItem !== "") {
       setToDoItem([
@@ -29,27 +27,54 @@ export default function TODO() {
     } else alert("Invalid");
   };
 
+  //Clear All Tasks
   const ClearAll = () => {
     setToDoItem([]);
   };
 
+  //Assigning input value to ToDoItem
   const onChangeEvent = (e) => {
     setNewToDoItem(e.target.value);
   };
 
+  //Clear Task
   const Clear = (id1) => {
     const newList = ToDoItems.filter((taskitem) => taskitem.id !== id1);
     setToDoItem(newList);
   };
 
+  //Move Task Up
   const Up = (u) => {
     const ind = ToDoItems.findIndex((taskitem) => taskitem.id === u);
-    let tempObj = ToDoItems[ind - 1];
-    ToDoItems[ind - 1] = ToDoItems[ind];
-    ToDoItems[ind] = tempObj;
+    if (ind !== 0) {
+      let tempObj = ToDoItems[ind - 1];
+      ToDoItems[ind - 1] = ToDoItems[ind];
+      ToDoItems[ind] = tempObj;
+    }
     setToDoItem([...ToDoItems]);
   };
 
+  //Move Task Down
+  const Down = (d) => {
+    const ind = ToDoItems.findIndex((taskitem) => taskitem.id === d);
+
+    if (ind !== ToDoItems.length - 1) {
+      let tempObj = ToDoItems[ind + 1];
+      ToDoItems[ind + 1] = ToDoItems[ind];
+      ToDoItems[ind] = tempObj;
+    }
+
+    //Alternate methods
+
+    //ToDoItems[ind] = ToDoItems.splice(ind+1, 1, ToDoItems[ind])[0];
+
+    // let tempObj = ToDoItems.splice(ind, 1, ToDoItems[ind+1])[0];
+    // ToDoItems.splice(ind+1, 1, tempObj);
+
+    setToDoItem([...ToDoItems]);
+  };
+
+  //Changing Task Status
   const Status = (a) => {
     const ind = ToDoItems.findIndex((taskitem) => taskitem.id === a);
 
@@ -59,43 +84,30 @@ export default function TODO() {
       ToDoItems[ind].isActive = true;
     }
 
-    console.log(ToDoItems[ind]);
-    setToDoItem([...ToDoItems]);
-  };
-  const Down = (d) => {
-    const ind = ToDoItems.findIndex((taskitem) => taskitem.id === d);
-
-    /*ToDoItems[ind] = ToDoItems.splice(ind+1, 1, ToDoItems[ind])[0];*/
-
-    /*let tempObj = ToDoItems.splice(ind, 1, ToDoItems[ind+1])[0];
-        ToDoItems.splice(ind+1, 1, tempObj);*/
-
-    let tempObj = ToDoItems[ind + 1];
-    ToDoItems[ind + 1] = ToDoItems[ind];
-    ToDoItems[ind] = tempObj;
-
-    console.log(ToDoItems[ind]);
     setToDoItem([...ToDoItems]);
   };
 
   return (
     <>
       <div className="background">
-        <h1>TODO LIST</h1>
+        <h1 className="heading">TODO LIST</h1>
+
         <InputView
           className="input"
           value={newToDoItem}
           placeholder={"Enter Text here"}
           onChange={onChangeEvent}
         />
+
         <Button name={"Add"} onClick={Add} />
+
         <Button name={"Clear All"} onClick={ClearAll} />
       </div>
 
       {ToDoItems.map((tasks) => {
         return (
           <div
-            className={tasks.isActive ? "list" : "inActiveTask"}
+            className={tasks.isActive ? "list" : "inActiveTask"} //Class Selection
             name="list"
             key={tasks.id}
           >
